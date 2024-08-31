cf20ee3a2a4d3f1e1d496e5a9ba6a66c488e2eeb
Tre Blackman <trebman91@gmail.com>
2024-08-31 18:15:01 -0400

    Calculator complete

--- lesson_2/practice_001_python_as_calculator.py
+++ lesson_2/practice_001_python_as_calculator.py
@@ -1,16 +1,16 @@
 # Try the following expressions in Terminal and share the results in the chat.
 
-# 1 + 1
-# 50 - 90
-# 28 * 3
-# -1 * 5
-# 478 / 2
-# 2 ** 8
-# 332 + 75.6
-# 500 // 2 ** 3
-# -5 + 2
-# 100 % 3
-# 100 // 3
-# 1 / 3
+print(1 + 1)
+print(50 - 90)
+print(28 * 3)
+print(-1 * 5)
+print(478 / 2)
+print(2 ** 8)
+print(332 + 75.6)
+print(500 // 2 ** 3)
+print(-5 + 2)
+print(100 % 3)
+print(100 // 3)
+print(1 / 3)
 
 # If more time is available, try different values and see what happens
