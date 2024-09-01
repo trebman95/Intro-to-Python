8083a98c9b613ced47e435d850625deae7c139a5
Tre Blackman <trebman91@gmail.com>
2024-08-31 20:27:55 -0400

    done

--- lesson_2/homework.py
+++ lesson_2/homework.py
@@ -199,7 +199,32 @@ print(number)  # will print 159
 
 # Print the result for all four digits
 
-###print(digit_1)
-###print(digit_2)
-###print(digit_3)
-###print(digit_4)
+
+digit_1 = number % 10
+number = number // 10
+
+digit_2 = number % 10
+number = number // 10
+
+digit_3 = number % 10
+number = number // 10
+
+digit_4 = number % 10
+number = number // 10
+
+print(digit_1)
+print(digit_2)
+print(digit_3)
+print(digit_4)
+
+###ALSO###
+
+#digit_1 = number % 10
+#digit_2 = (int(number % 100 / 10))
+#digit_3 = (int(number % 1000/ 100))
+#digit_4 = (int(number % 10000/ 1000))
+
+#print(digit_1)
+#print(digit_2)
+#print(digit_3)
+#print(digit_4)
--- lesson_2/sandbox.py
+++ lesson_2/sandbox.py
@@ -1,5 +1,28 @@
+number = 1597
 
-original_price = 80
-discounted_percentage = 0.20
-discounted_price = (original_price * discounted_percentage) / 100
-print (discounted_price)
+#digit_1 = number % 10
+#digit_2 = (int(number % 100 / 10))
+#digit_3 = (int(number % 1000/ 100))
+#digit_4 = (int(number % 10000/ 1000))
+
+#print(digit_1)
+#print(digit_2)
+#print(digit_3)
+#print(digit_4)
+
+digit_1 = number % 10
+number = number // 10
+
+digit_2 = number % 10
+number = number // 10
+
+digit_3 = number % 10
+number = number // 10
+
+digit_4 = number % 10
+number = number // 10
+
+print(digit_1)
+print(digit_2)
+print(digit_3)
+print(digit_4)
