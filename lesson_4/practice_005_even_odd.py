853341c314f997440cddb88e8f1fc0a7f472a8db
Tre Blackman <trebman91@gmail.com>
2024-09-26 00:47:41 -0400

    finished practices

--- lesson_4/practice_005_even_odd.py
+++ lesson_4/practice_005_even_odd.py
@@ -1,6 +1,11 @@
 # Create a variable called number. Set it to any number.
-
+number = int(input('Enter a number: '))
 # Using if else statement, create the logic that will print "Even" for even numbers or "Odd" for odd numbers.
 
 # Odd numbers are: 1, 3, 5, 7, etc.
 # Even: 2, 4, 6, etc.
+
+if number % 2 == 0:
+    print("Even")
+else:
+    print ("Odd")
--- lesson_4/practice_006_fizzbuzz.py
+++ lesson_4/practice_006_fizzbuzz.py
@@ -1,4 +1,21 @@
 # Create a variable called number. Set it to any integer.
+number = int(input('Enter an integer: '))
 # If the number is divisible by 3, print 'Fizz'
 # If the number is divisible by 5, print 'Buzz'
 # If the number is divisible by both 3 and 5, print 'FizzBuzz'
+
+#if number // 3:
+#    print('Fizz')
+#    if number // 5:
+#       print('Buzz')
+#       if number // 3 and number // 5:
+#          print('FizzBuzz')
+
+if number % 3 == 0 and number % 5 == 0:
+    print('FizzBuzz')
+elif number % 3 == 0:
+    print('Fizz')
+elif number % 5 == 0:
+    print('Buzz')
+
+
