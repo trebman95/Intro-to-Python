1465cfb4a80a3d56c40d723d20ad68700b36e47d
Tre Blackman <trebman91@gmail.com>
2024-09-29 21:15:51 -0400

    Finished practice 1

--- lesson_8/practice_001_sum_and_multiplication.py
+++ lesson_8/practice_001_sum_and_multiplication.py
@@ -6,3 +6,18 @@
 # Output sum: 10
 # Output multiplication: 24
 
+
+numbers = [1, 2, 3, 4]
+sum_result = 0
+mult_result = 1
+
+for number in numbers:
+    sum_result = sum_result + number
+    mult_result = mult_result * number
+
+print(numbers)
+print(f'Output sum: {sum_result}')
+print(f'Output mult: {mult_result}')
+
+
+
