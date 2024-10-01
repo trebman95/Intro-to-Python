96d82d5459dd843a5fb95e7c1c2d14188fedebab
Tre Blackman <trebman91@gmail.com>
2024-09-30 23:38:49 -0400

    practice 2 done

--- lesson_8/practice_002_remove_vowels.py
+++ lesson_8/practice_002_remove_vowels.py
@@ -1,9 +1,17 @@
 # Remove the lowercase vowels in a given string:
-# str1 = ‘Hello, World!’
+#str1 = 'Hello, World'
+str1 = input('Enter a string: ')
 # The vowels are:
-# vowels = "aeiou"
+vowels = "aeiou"
 # “y” is not considered a vowel for this task. The input string is always in lowercase.
 # Examples:
 # "hello" --> "hll"
 # "goodbye" --> "gdby"
 
+result = ''
+
+for string in str1:
+    if string not in vowels:
+        result += string
+
+print(result)
