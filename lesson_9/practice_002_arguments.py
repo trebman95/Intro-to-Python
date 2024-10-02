ac2c0d37be044c8329305a29c79d113280105bed
Tre Blackman <trebman91@gmail.com>
2024-10-02 08:49:44 -0400

    Practice  2 done

--- lesson_9/practice_002_arguments.py
+++ lesson_9/practice_002_arguments.py
@@ -1,8 +1,14 @@
 # Create a function that prints the full name using
 # the provided arguments.
 
-def ???(???, ???):
-   full_name = ???
+#def print_full_name(name1, name2):
+#   full_name = name1 + " " + name2
+#   print(full_name)
+
+#print_full_name("Nelson", "Mandela")
+
+def print_full_name(first_name, last_name):
+   full_name = f'{first_name} {last_name}'
    print(full_name)
 
 print_full_name("Nelson", "Mandela")
