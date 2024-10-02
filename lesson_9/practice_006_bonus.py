ee0b162a6fdc90d13d34e6f2cb47551f68ec2853
Tre Blackman <trebman91@gmail.com>
2024-10-02 09:59:01 -0400

    Finished all practices

--- lesson_9/practice_006_bonus.py
+++ lesson_9/practice_006_bonus.py
@@ -6,3 +6,24 @@
 #    - A line with the first letter of the user's name: "Your initial is {initial}."
 #
 # Use at least one function. Extra points if you use two and call a function from another function.
+
+def print_messages(name):
+    initial = name[0:1]
+    print(f'Hello {name}!')
+    print(f'Your initial is {initial}')
+
+def process_names():
+ names = []
+
+ while True:
+     name = input('What is your name: ')
+
+     if name == 'end':
+         break
+
+     names.append(name)
+
+ for name in names:
+     print_messages(name)
+
+process_names()
