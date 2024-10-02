4ec13b4f0a4c8fbec0724e09b8080338109ad739
Tre Blackman <trebman91@gmail.com>
2024-10-02 08:45:47 -0400

    Practice 1 done

--- lesson_9/practice_001_greeting.py
+++ lesson_9/practice_001_greeting.py
@@ -1,6 +1,19 @@
 # The following program takes a user's name and greets them back.
 # Modify the code to use a function to print the greeting instead.
 
-user_name = input("What's your name: ")
-greet = f"Hello {user_name}!"
-print(greet)
+#user_name = input("What's your name: ")
+#greet = f"Hello {user_name}!"
+#print(greet)
+
+#def greet():
+#    user_name = input('What is your name: ')
+#    print(f'Hello {user_name}!')
+#greet()
+
+def greet(name):
+    print(f'Hello {name}!')
+
+user_name = input('What is your name: ')
+greet(user_name)
+
+
